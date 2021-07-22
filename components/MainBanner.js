7acf8d2a80c8b78eb51c722ccb5bd18cb3af504a
Craig <craig@craigsteel-design.com>
2021-07-22 14:06:43 +0100

    changes to banner

--- components/MainBanner.js
+++ components/MainBanner.js
@@ -7,8 +7,8 @@ const MainBanner = () => {
         Hi, I'm Craig and thanks for visiting my portfolio which focuses on UI/UX Design.
       </h1>
       <h2 className="banner__main--intro">
-        I am a multidisciplinary UI/UX creative based UK.
-        With a background of over 20 years in branding, corporate and web design. I bring a wealth of Experience knowledge that I can alongside clients identify problems, solve them and create a better and more enjoyable online user experience.
+        I am a multidisciplinary UI/UX creative based in the UK.
+        With a background of over 20 years in branding, corporate and web design, I bring a wealth of experience and knowledge to each project that can help clients to identify and solve their communication problems, creating a better and more enjoyable online user experience.
       </h2>
       <div className="banner__main--projects">
         <h3>Featured Projects</h3>
