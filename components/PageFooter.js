5a18aaa219998029913a8af45eebac26b64ff143
Craig Steel <craig@craigsteel-design.com>
2021-09-05 13:02:36 +0100

    changes to footer

--- components/PageFooter.js
+++ components/PageFooter.js
@@ -8,12 +8,15 @@ const PageFooter = () =>
     <div className="footer__disc">
       APPLICATION designed & built using - Pen & Paper - SKETCH - NODE.js - NEXT.js - SANITY - vercel
     </div>
+    <div className="footer__name--link">
+    <a href="https://craigsteel-design.com/">VISIT craigsteel-design.com TO VIEW A LARGER SELECTION OF MY BRANDING, PACKAGING, PRINT AND WEB APPLICATION PROJECTS </a>
+    </div>
     <a href="/">home</a>{' | '}
     <a href="https://github.com/craigsteel/csportfolio">github</a>{' | '}
     <a href="https://twitter.com/home">twitter</a>{' | '}
     <a href="mailto:craig@craig-steel-design.com"
       target="_blank"
-      rel="noopener noreferrer">Contact
+      rel="noopener noreferrer">contact
     </a>
   </footer>
 
