f999714ed1ca61a159db9319b97adf4f327b27d9
craigsteel <craig@craigsteel-design.com>
2023-04-01 01:02:25 +0100

    changes to schema

--- schemas/schema.js
+++ schemas/schema.js
@@ -13,9 +13,9 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     {
-      name: 'work',
+      name: 'projects',
       type: 'document',
-      title: 'work',
+      title: 'projects',
       fields: [
         {
           name: 'number',
