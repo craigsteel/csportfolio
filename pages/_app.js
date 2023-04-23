96b55b872a59ed697abe1535a3e7a63659cfd7cc
craigsteel <craig@craigsteel-design.com>
2023-04-23 22:46:15 +0100

    changes to files

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'highlight.js/styles/darcula.css';
 import 'styles/styles.scss';
+import { Analytics } from "@vercel/analytics/react";
 import { AnimatePresence } from 'framer-motion';
 
-const MyBlog = ({ Component, pageProps }) =>
-<AnimatePresence exitBeforeEnter>
-    <Component
-    {...pageProps}
-  />;
-</AnimatePresence>
+const MyBlog = ({ Component, pageProps }) => (
+  <AnimatePresence exitBeforeEnter>
+    <Component {...pageProps} />
+    <Analytics />
+  </AnimatePresence>
+);
 
-
-  export default MyBlog;
+export default MyBlog;
