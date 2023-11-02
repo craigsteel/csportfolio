f95e110381b29b582694fa3d3c535178f4512cfa
craigsteel <craig@craigsteel-design.com>
2023-11-02 08:03:00 +0000

    updated about

--- components/AboutBanner.js
+++ components/AboutBanner.js
@@ -2,7 +2,7 @@
 const AboutBanner = () =>
   <div className="banner banner__about">
     <h1 className="banner__about--heading mb-3">
-      Hi, I'm Craig and thanks for visiting my portfolio which focuses on UI/UX Design.
+      Hi, I'm Craig and thank you for visiting my portfolio which focuses on UI/UX Design.
     </h1>
       <h2 className="banner__about--intro">
         My passion for design has enabled me to pursue a career that I love, whether in  advertising with its dynamic pace of the studio, deadlines and creative teams, to the corporate world of annual reporting, strategic marketing of brands to UX/UI Design and Development of web applications.
